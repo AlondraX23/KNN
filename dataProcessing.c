d4f08d1f86fe71bba6630120e4b0a9dc4c465a0b
Alondra <axdamian.23@gmail.com>
2025-09-28 02:32:08 -0600

    Ingreso de Puntos,  uso de hypot(). Arreglar  -lm

--- dataProcessing.c
+++ dataProcessing.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <math.h>
+
 typedef struct
 {
   int x, y, c;
 } P;
 
+void myFunction(P *arr, int lineas, P *puntos)
+{
+  // int puntoComparacion_x = 1; // coordenada de prueba
+  // int puntoComparacion_y = 2; // coordenada de prueba
+
+  for (int i = 0; i < lineas; i++)
+  { // agregar double para obtener resultados correctos
+    int x = arr[i].x - puntos[i].x;
+    int y = arr[i].y - puntos[i].x;
+
+    int distancia = hypot(x, y);
+
+    printf("suma de las x y %d\n", distancia);
+  }
+}
+
 int main()
 {
-  P arr[4];
-
-  // Selección de archivo de datos
-  FILE *f = fopen("data.txt", "r");
+  P arr[1];
+  // Selección de archivo de CLASES
+  FILE *f = fopen("clases.txt", "r");
   if (!f)
   {
     printf("Archivo no existe o la dirección es incorrecta");
@@ -24,15 +40,49 @@ int main()
     a++;
   }
   {
-    printf("Datos leidos\n\n");
+    printf("Datos leidos, %d coordenadas\n\n", a);
   }
 
-  // Imprimir coordenadas y clase
+  // numero de líneas en el archivo
+  int lineas = a;
 
+  // Imprimir coordenadas y clase
   for (int i = 0; i < a; ++i)
   {
     printf("%d %d %d \n", arr[i].x, arr[i].y, arr[i].c);
   }
+
+  //-------------------------------------------------------
+
+  P puntos[1];
+  // Selección de archivo de PUNTOS
+  FILE *g = fopen("puntos.txt", "r");
+  if (!g)
+  {
+    printf("Archivo no existe o la dirección es incorrecta");
+    return 1;
+  }
+
+  // Lectura de datos
+  int b = 0;
+  while (fscanf(g, "%d %d ", &puntos[b].x, &puntos[b].y) == 2)
+  {
+    b++;
+  }
+  {
+    printf("Datos leidos, %d coordenadas\n\n", b);
+  }
+
+  // numero de líneas en el archivo
+  int lines = b;
+
+  // Imprimir coordenadas y clase
+  for (int j = 0; j < b; ++j)
+  {
+    printf("%d %d \n", puntos[j].x, puntos[j].y);
+  }
+
+  myFunction(arr, lineas, puntos);
 
   return 0;
 }
