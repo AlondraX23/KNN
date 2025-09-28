0456eb93e944034311d31b0fd5f05153eb3564d7
Alondra <axdamian.23@gmail.com>
2025-09-27 23:37:10 -0600

    Ingreso y lectura de datos

--- dataProcessing.c
+++ dataProcessing.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <math.h>
+typedef struct
+{
+  int x, y, c;
+} P;
 
-int main() {
-  printf("Hello World!");
+int main()
+{
+  P arr[4];
+
+  // Selección de archivo de datos
+  FILE *f = fopen("data.txt", "r");
+  if (!f)
+  {
+    printf("Archivo no existe o la dirección es incorrecta");
+    return 1;
+  }
+
+  // Lectura de datos
+  int a = 0;
+  while (fscanf(f, "%d %d %d ", &arr[a].x, &arr[a].y, &arr[a].c) == 3)
+  {
+    a++;
+  }
+  {
+    printf("Datos leidos\n\n");
+  }
+
+  // Imprimir coordenadas y clase
+
+  for (int i = 0; i < a; ++i)
+  {
+    printf("%d %d %d \n", arr[i].x, arr[i].y, arr[i].c);
+  }
+
   return 0;
-} 
+}
