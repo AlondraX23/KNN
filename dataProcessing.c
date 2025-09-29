937f90620f6aa0281bd3fefc9ed07997d55ce9f9
Alondra <axdamian.23@gmail.com>
2025-09-28 23:10:50 -0600

    Correcto cálculo con hypot(), formato de texto entendible .

--- dataProcessing.c
+++ dataProcessing.c
@@ -3,84 +3,87 @@
 
 typedef struct
 {
-  int x, y, c;
+  int x, y;
+  char c;
 } P;
 
-void myFunction(P *arr, int lineas, P *puntos)
+void myFunction(P *arr, int lineas, P *puntos) // Crear ciclo para que que se evalúe la distancia con los demás puntos. usar lines
 {
-  // int puntoComparacion_x = 1; // coordenada de prueba
-  // int puntoComparacion_y = 2; // coordenada de prueba
-
+  printf("\n*******************************************************************\n\n");
   for (int i = 0; i < lineas; i++)
-  { // agregar double para obtener resultados correctos
-    int x = arr[i].x - puntos[i].x;
-    int y = arr[i].y - puntos[i].x;
+  {
 
-    int distancia = hypot(x, y);
+    double x = arr[i].x - puntos[0].x;
+    double y = arr[i].y - puntos[0].y;
 
-    printf("suma de las x y %d\n", distancia);
+    double distancia = hypot(x, y);
+
+    printf("  La distancia entre los puntos (%d, %d) y (%d, %d) es %lf\n", arr[i].x, arr[i].y, puntos[0].x, puntos[0].y, distancia);
   }
+  printf("\n*******************************************************************\n");
 }
 
 int main()
 {
-  P arr[1];
+  P arr[10];
   // Selección de archivo de CLASES
   FILE *f = fopen("clases.txt", "r");
   if (!f)
   {
-    printf("Archivo no existe o la dirección es incorrecta");
+    printf("El archivo de COORDENADAS no existe o la dirección es incorrecta");
     return 1;
   }
-
-  // Lectura de datos
+  printf("________________________________________\n\n");
+  // Lectura de coordenadas
   int a = 0;
-  while (fscanf(f, "%d %d %d ", &arr[a].x, &arr[a].y, &arr[a].c) == 3)
+  while (fscanf(f, "%d %d %c ", &arr[a].x, &arr[a].y, &arr[a].c) == 3)
   {
     a++;
   }
   {
-    printf("Datos leidos, %d coordenadas\n\n", a);
+    printf("  COORDENADAS: %d datos leidos \n\n", a);
   }
 
-  // numero de líneas en el archivo
+  // Numero de líneas en el archivo
   int lineas = a;
 
   // Imprimir coordenadas y clase
   for (int i = 0; i < a; ++i)
   {
-    printf("%d %d %d \n", arr[i].x, arr[i].y, arr[i].c);
+    printf("\t(%d, %d) \tclase: %c \n", arr[i].x, arr[i].y, arr[i].c);
   }
+  printf("\n________________________________________\n");
 
-  //-------------------------------------------------------
+  //--------------------------------------------------------------------------------------
 
   P puntos[1];
   // Selección de archivo de PUNTOS
   FILE *g = fopen("puntos.txt", "r");
   if (!g)
   {
-    printf("Archivo no existe o la dirección es incorrecta");
+    printf("Archivo de PUNTOS DE COMPARACIÓN no existe o la dirección es incorrecta");
     return 1;
   }
 
-  // Lectura de datos
+  // Lectura de punto
   int b = 0;
   while (fscanf(g, "%d %d ", &puntos[b].x, &puntos[b].y) == 2)
   {
     b++;
   }
   {
-    printf("Datos leidos, %d coordenadas\n\n", b);
+    printf("\n  PUNTOS: %d datos leidos\n\n", b);
   }
 
-  // numero de líneas en el archivo
+  // Numero de líneas en el archivo
   int lines = b;
 
   // Imprimir coordenadas y clase
   for (int j = 0; j < b; ++j)
   {
-    printf("%d %d \n", puntos[j].x, puntos[j].y);
+    printf("\t(%d, %d) \n", puntos[j].x, puntos[j].y);
   }
+  printf("________________________________________\n");
 
   myFunction(arr, lineas, puntos);
 
